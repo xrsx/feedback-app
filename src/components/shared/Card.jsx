0afa5aabdb3271fe0971a06875a5a95b3404b63c
xrsx <021novisad@gmail.com>
2022-06-23 18:09:08 +0200

    Update

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,27 +1,28 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types'
 
 function Card({ children, reverse }) {
-  //  return <div className={`card ${reverse && "reverse"}`}>{children}</div>;
+  // return <div className={`card ${reverse && 'reverse'}`}>{children}</div>
+
   return (
     <div
-      className="card"
+      className='card'
       style={{
-        backgroundColor: reverse ? "rgba(0,0,0,0.4)" : "#fff",
-        color: reverse ? "#fff" : "#000"
+        backgroundColor: reverse ? 'rgba(0,0,0,0.4)' : '#fff',
+        color: reverse ? '#fff' : '#000',
       }}
     >
       {children}
     </div>
-  );
+  )
 }
 
 Card.defaultProps = {
-  reverse: false
-};
+  reverse: false,
+}
 
 Card.propTypes = {
   children: PropTypes.node.isRequired,
-  reverse: PropTypes.bool
-};
+  reverse: PropTypes.bool,
+}
 
-export default Card;
+export default Card
--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
